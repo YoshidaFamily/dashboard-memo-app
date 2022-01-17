62d23d52f7c566ec88070c879c18e77e8996875e
123yoshidandy <56680821+123yoshidandy@users.noreply.github.com>
2022-01-17 18:00:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
+var memoElement = document.getElementById("field_table");
 
+init();
+
+function init() {
+}
+
+function onClick(event) {
+    var x = event.target.cellIndex;
+    var y = event.target.parentElement.rowIndex;
+}
+
+function addCategory() {
+}
+
+function editCategory() {
+}
+
+function delelteCategory() {
+}
+
+function addMemo() {
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+
+    if(document.addEventListener){
+        td.addEventListener("click" , onClick);
+    }else if(document.attachEvent){
+        td.attachEvent("onclick" , onClick);
+    }
+
+    tr.appendChild(td);
+    memoElement.appendChild(tr);
+}
+
+function editMemo() {
+}
+
+function delelteMemo() {
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+
+    if(document.addEventListener){
+        td.addEventListener("click" , onClick);
+    }else if(document.attachEvent){
+        td.attachEvent("onclick" , onClick);
+    }
+
+    tr.appendChild(td);
+    memoElement.appendChild(tr);
+}
